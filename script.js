a370cb46bfaef575b79bc78c9197c92da27c8856
brco <brandoncorness@gmail.com>
2014-07-05 13:16:07 -0700

    Added js

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	var $ingredients = $('ul li');
+	$ingredients.click(function() {
+		$(this).css("text-decoration", "line-through");
+		$(this).fadeTo("slow", 0.5);
+	});
+	var $directions = $('ol li');
+	$directions.click(function() {
+		$(this).css("text-decoration", "line-through");
+		$(this).fadeTo("fast", 0.25);
+	});
+	$('h6').click(function() {
+		$('img').fadeOut('slow')
+	});
+});
